66d69ef969440afc2ed3e8b5f36e914d81866a02
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-07-12 20:22:00 +0530

    Setup Clerk Configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,21 +9,23 @@ import Community from './pages/Community'
 import ReviewResume from './pages/ReviewResume'
 import RemoveBackground from './pages/RemoveBackground'
 import GenerateImages from './pages/GenerateImages'
+import RemoveObject from './pages/RemoveObject'
 
 function App() {
 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ai" element={<Layout />} >
+        <Route path='/' element={<Home />}>
+          <Route path='/ai' element={<Layout />} />
           <Route index element={<Dashboard />} />
-          <Route path="write-article" element={<WriteArticle />} />
-          <Route path="blog-titles" element={<BlogTitles />} />
-          <Route path="generate-images" element={<GenerateImages />} />
-          <Route path="remove-background" element={<RemoveBackground />} />
-          <Route path="review-resume" element={<ReviewResume />} />
-          <Route path="community" element={<Community />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='community' element={<Community />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='generate-images' element={<GenerateImages />} />
         </Route>
       </Routes>
     </div>
