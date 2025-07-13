82ebdc0d0c62a168c3c446bcb2e4bd72a4663644
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-07-13 13:25:37 +0530

    Testimonial & clerk plan card integration

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
     <Navbar/>
     <Hero/>
     <AiTools/>
+    <Testimonial/>
+    <Plan/>
     </>
   )
 }
